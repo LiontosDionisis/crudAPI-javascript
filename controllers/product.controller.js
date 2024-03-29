1e77e0f5483cb5f2cc6cfd1fce2b019e5998871c
Rmoney <DionisisLiontos@outlook.com>
2024-03-29 05:57:06 +0200

    made all https methods for products

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -15,3 +15,75 @@ exports.findAll = async(req, res) => {
         logger.error("Error in finding all products");
     }
 }
+
+exports.findOne = async(req, res) => {
+    console.log("Find one product");
+    const id = req.params.id;
+
+    try {
+        const result = await product.findOne({_id: id});
+        res.status(200).json({data: result});
+        logger.info("Found 1 product");
+        console.log("Found 1 product");
+    } catch (err) {
+        res.status(404).json({data: err});
+        logger.error("No product was found");
+        console.log("No product was found")
+    }
+}
+
+exports.create = async(req, res) => {
+    const newProduct = new product({
+        product: req.body.product,
+        cost: req.body.cost,
+        description: req.body.description,
+        quantity: req.body.quantity
+    })
+
+    try {
+        const result = await newProduct.save();
+        res.status(200).json({data: result});
+        logger.info("Product created");
+    } catch(err) {
+        res.status(404).json({data: err});
+        logger.error("Error in creating product")
+    }
+}
+
+exports.update = async(req, res) => {
+    const id = req.params.id
+    const updateProduct = {
+        product: req.body.product,
+        cost: req.body.cost,
+        description: req.body.description,
+        quantity: req.body.quantity
+    }
+
+    try {
+        const result = await product.findOneAndUpdate(
+            {_id: id},
+            updateProduct,
+            {new: true}
+        )
+        res.status(200).json({data: result});
+        logger.info("Product updated");
+    } catch (err) {
+        res.status(404).json({data: err});
+        logger.error("Error in updating product")
+    }
+}
+
+exports.delete = async(req, res) => {
+    const id = req.params.id;
+
+    try {
+        const result = await product.findOneAndDelete(
+            {_id: id}
+        )
+        res.status(200).json({data: result});
+        logger.info("Product deleted");
+    } catch(err) {
+        res.status(404).json({data: err})
+        logger.error("Error in deleting")
+    }
+}
--- routes/product.route.js
+++ routes/product.route.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const productController = require("../controllers/product.controller");
 
 router.get("/", productController.findAll);
+router.get("/:id", productController.findOne);
+router.post("/", productController.create);
+router.patch("/:id", productController.update);
+router.delete("/:id", productController.delete);
 
 module.exports = router;
